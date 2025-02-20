178969bcae5666005e8905d61fe690e4d0a17c88
CARLOS ALEXANDRE STADNICK <136737657+CarlosStadnick@users.noreply.github.com>
2025-02-19 21:50:49 -0300

    aula 19/02
    andamento do projeto sa4

--- server/api.js
+++ server/api.js
@@ -1,10 +1,8 @@
- 
 const express = require('express');
 const mysql = require('mysql2');
 const cors = require('cors');
 const app = express();
 const port = 3000;
-const bcrypt = require('bcrypt');
 
 app.use(express.json());
 app.use(cors());
@@ -14,7 +12,7 @@ const db = mysql.createConnection({
     host: 'localhost',
     user: 'root', // Ajuste conforme necessário
     password: '', // Ajuste conforme necessário
-    database: 'sa4'
+    database: 'teste_sistemas'
 });
 
 // Conectar ao banco de dados
@@ -26,35 +24,19 @@ db.connect(err => {
     }
 });
 
-// Rota de teste
-app.get('/', (req, res) => {
-    res.send('API de Automação de Solicitação de Compras');
-});
-
-// Rota de Login
+// Rota de login
 app.post('/login', (req, res) => {
     const { nome_usuario, senha_usuario } = req.body;
     
-    db.query('SELECT * FROM usuario WHERE nome_usuario = ?', [nome_usuario], (err, results) => {
+    db.query('SELECT * FROM usuarios WHERE usuario = ? AND senha = ?', [nome_usuario, senha_usuario], (err, results) => {
         if (err) return res.status(500).json({ error: err.message });
         
         if (results.length === 0) {
             return res.status(401).json({ message: 'Usuário ou senha inválidos!' });
         }
 
-        const user = results[0];
-
-        // Comparar a senha fornecida com a senha criptografada no banco de dados usando bcrypt
-        bcrypt.compare(senha_usuario, user.senha_usuario, (err, isMatch) => {
-            if (err) return res.status(500).json({ error: 'Erro ao verificar a senha' });
-            
-            if (isMatch) {
-                // Login bem-sucedido
-                res.json({ message: 'Login realizado com sucesso!', redirect: '/produtos.html' });
-            } else {
-                res.status(401).json({ message: 'Usuário ou senha inválidos!' });
-            }
-        });
+        // Login bem-sucedido
+        res.json({ message: 'Login realizado com sucesso!', redirect: 'tela_inicial.html' });
     });
 });
 
